13b353978840724c15f24ac22daf4219dfb904b4
App Academy Student <Bojan.Bajovic@gmail.com>
2018-07-31 17:33:28 -0400

    Step towards pawn completion

--- Chess/pieces/pawn.rb
+++ Chess/pieces/pawn.rb
@@ -2,4 +2,41 @@ require_relative 'piece'
 
 class Pawn < Piece
 
+  attr_accessor :moved
+
+
+
+  def symbol
+    "P"
+  end
+
+  def move_dir
+
+  end
+
+  private
+
+  def at_start_row?
+    (pos[1] == 1 && color == :white) || (pos[1] == 6 && color == :black )
+  end
+
+  def forward_dir
+    if color == :white
+      return -1
+    else
+      return 1
+    end
+  end
+
+  def forward_steps
+    #pos[0 + forward_dir]
+
+  end
+
+  def side_attacks
+
+
+  end
+
+
 end
